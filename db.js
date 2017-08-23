fd685dd6b43a868549f20da22b87ffdca20438fe
vladimir.uram <vladimir.uram@globallogic.com>
2017-08-23 12:37:53 +0200

    database changed to mongodb

--- db.js
+++ db.js
@@ -1,11 +1,29 @@
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database : process.env.DB_NAME
-})
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database : process.env.DB_NAME
+// })
 
-connection.connect()
+// connection.connect()
 
-module.exports = connection;
+// module.exports = connection;
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_HOST+"/"+process.env.DB_NAME;
+var _db;
+
+module.exports = {
+
+connectToServer: function( callback ) {
+  MongoClient.connect( url, function( err, db ) {
+    _db = db;
+    return callback( err );
+  } );
+},
+
+getDb: function() {
+  return _db;
+}
+};
